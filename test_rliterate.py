b5d579b03787c5f2da67baab17c8ca584217b00e
Rickard Lindberg <ricli85@gmail.com>
2018-08-04 15:06:34 +0200

    Better document and test legacy documents.

--- test_rliterate.py
+++ test_rliterate.py
@@ -11,90 +11,117 @@ def tmpfile(tmpdir):
     return str(tmpdir.join("test.rliterate"))
 
 
-def test_can_read_legacy_file_format_paragraph_had_text(tmpfile):
-    text = "This **is** very *cool*. [[106af6f8665c45e8ab751993a6abc876:page]] is it. Some `code`. [link](http://example.com)."
-    fragments = [
-        {"type": "text",      "text": "This "},
-        {"type": "strong",    "text": "is"},
-        {"type": "text",      "text": " very "},
-        {"type": "emphasis",  "text": "cool"},
-        {"type": "text",      "text": ". "},
-        {"type": "reference", "text": "page", "page_id": "106af6f8665c45e8ab751993a6abc876"},
-        {"type": "text",      "text": " is it. Some "},
-        {"type": "code",      "text": "code"},
-        {"type": "text",      "text": ". "},
-        {"type": "link",      "text": "link", "url": "http://example.com"},
-        {"type": "text",      "text": "."},
-    ]
-    write_json_to_file(tmpfile, {
-        "title": "Root",
-        "children": [],
-        "id": "106af6f8665c45e8ab751993a6abc876",
-        "paragraphs": [
-            {"id": "abc1", "type": "text", "text": text},
-            {"id": "abc2", "type": "quote", "text": text},
-            {"id": "abc3", "type": "image", "text": text, "image_base64": "data"},
-            {"id": "abc4", "type": "list", "text": "\n".join([
-                "* **a**{}".format(text),
-                "* **b**{}".format(text),
-                "    1. **c**{}".format(text),
-                "    1. **d**{}".format(text),
-            ])},
-        ],
-    })
-    doc = Document.from_file(tmpfile)
-    with doc.notify():
-        pass
-    assert load_json_from_file(tmpfile) == {
-        "root_page": {
-            "title": "Root",
+INLINE_TEXT = "This **is** very *cool*. [[106af6f8665c45e8ab751993a6abc876:page]] is it. Some `code`. [link](http://example.com)."
+FRAGMENTS = [
+    {"type": "text",      "text": "This "},
+    {"type": "strong",    "text": "is"},
+    {"type": "text",      "text": " very "},
+    {"type": "emphasis",  "text": "cool"},
+    {"type": "text",      "text": ". "},
+    {"type": "reference", "text": "page", "page_id": "106af6f8665c45e8ab751993a6abc876"},
+    {"type": "text",      "text": " is it. Some "},
+    {"type": "code",      "text": "code"},
+    {"type": "text",      "text": ". "},
+    {"type": "link",      "text": "link", "url": "http://example.com"},
+    {"type": "text",      "text": "."},
+]
+TEXT_OLD = {"id": "abc1", "type": "text", "text": INLINE_TEXT}
+TEXT_NEW = {"id": "abc1", "type": "text", "fragments": FRAGMENTS}
+QUOTE_OLD = {"id": "abc2", "type": "quote", "text": INLINE_TEXT}
+QUOTE_NEW = {"id": "abc2", "type": "quote", "fragments": FRAGMENTS}
+IMAGE_OLD = {"id": "abc3", "type": "image", "image_base64": "data",
+             "text": INLINE_TEXT}
+IMAGE_NEW = {"id": "abc3", "type": "image", "image_base64": "data",
+             "fragments": FRAGMENTS}
+LIST_OLD = {
+    "id": "abc4",
+    "type": "list",
+    "text": "\n".join([
+        "* **a**{}".format(INLINE_TEXT),
+        "* **b**{}".format(INLINE_TEXT),
+        "    1. **c**{}".format(INLINE_TEXT),
+        "    1. **d**{}".format(INLINE_TEXT),
+    ]),
+}
+LIST_NEW = {
+    "id": "abc4",
+    "type": "list",
+    "child_type": "unordered",
+    "children": [
+        {
+            "fragments": [{"type": "strong", "text": "a"}]+FRAGMENTS,
+            "child_type": None,
             "children": [],
-            "id": "106af6f8665c45e8ab751993a6abc876",
-            "paragraphs": [
-                {"id": "abc1", "type": "text", "fragments": fragments},
-                {"id": "abc2", "type": "quote", "fragments": fragments},
-                {"id": "abc3", "type": "image", "fragments": fragments, "image_base64": "data"},
-                {"id": "abc4", "type": "list", "child_type": "unordered", "children": [
-                    {"child_type": None, "children": [], "fragments": [{"type": "strong", "text": "a"}]+fragments},
-                    {"child_type": "ordered", "children": [
-                        {"child_type": None, "children": [], "fragments": [{"type": "strong", "text": "c"}]+fragments},
-                        {"child_type": None, "children": [], "fragments": [{"type": "strong", "text": "d"}]+fragments},
-                    ], "fragments": [{"type": "strong", "text": "b"}]+fragments},
-                ]},
+        },
+        {
+            "fragments": [{"type": "strong", "text": "b"}]+FRAGMENTS,
+            "child_type": "ordered",
+            "children": [
+                {
+                    "fragments": [{"type": "strong", "text": "c"}]+FRAGMENTS,
+                    "child_type": None,
+                    "children": [],
+                },
+                {
+                    "fragments": [{"type": "strong", "text": "d"}]+FRAGMENTS,
+                    "child_type": None,
+                    "children": [],
+                },
             ],
         },
-        "variables": {},
-    }
-def test_reads_legacy_code_paragraph(tmpfile):
-    write_json_to_file(tmpfile, {
+    ]
+}
+def test_converts_inline_text_to_fragments(tmpfile):
+    old_paragraphs = [TEXT_OLD, QUOTE_OLD, IMAGE_OLD, LIST_OLD]
+    new_paragraphs = [TEXT_NEW, QUOTE_NEW, IMAGE_NEW, LIST_NEW]
+    document = write_read_document(tmpfile, {
         "title": "Root",
-        "children": [],
-        "id": "106af6f8665c45e8ab751993a6abc876",
-        "paragraphs": [
+        "id": "abc1",
+        "paragraphs": old_paragraphs,
+        "children": [
             {
-                "id": "abc1",
-                "type": "code",
-                "path": ["foo.py", "<<functions>>"],
-                "text": "one\n<<chunk>>\ntwo\n",
+                "title": "Child",
+                "children": [],
+                "id": "abc2",
+                "paragraphs": old_paragraphs,
             }
         ],
     })
-    document = Document(tmpfile)
-    with document.notify():
-        pass
-    assert load_json_from_file(tmpfile)["root_page"]["paragraphs"] == [
-        {
-            "id": "abc1",
-            "type": "code",
-            "filepath": ["foo.py"],
-            "chunkpath": ["functions"],
-            "fragments": [
-                {"type": "code", "text": "one\n"},
-                {"type": "chunk", "prefix": "", "path": ["chunk"]},
-                {"type": "code", "text": "two\n"},
-            ],
-        }
-    ]
+    assert document["root_page"]["paragraphs"] == new_paragraphs
+    assert document["root_page"]["children"][0]["paragraphs"] == new_paragraphs
+CODE_OLD = {
+    "id": "abc1",
+    "type": "code",
+    "path": ["foo.py", "<<functions>>"],
+    "text": "one\n<<chunk>>\ntwo\n",
+}
+CODE_NEW = {
+    "id": "abc1",
+    "type": "code",
+    "filepath": ["foo.py"],
+    "chunkpath": ["functions"],
+    "fragments": [
+        {"type": "code", "text": "one\n"},
+        {"type": "chunk", "prefix": "", "path": ["chunk"]},
+        {"type": "code", "text": "two\n"},
+    ],
+}
+def test_converts_legacy_code_paragraphs(tmpfile):
+    document = write_read_document(tmpfile, {
+        "title": "Root",
+        "children": [
+            {
+                "title": "Child",
+                "children": [],
+                "id": "abc2",
+                "paragraphs": [CODE_OLD],
+            },
+        ],
+        "id": "abc1",
+        "paragraphs": [CODE_OLD],
+    })
+    assert document["root_page"]["paragraphs"] == [CODE_NEW]
+    assert document["root_page"]["children"][0]["paragraphs"] == [CODE_NEW]
 @pytest.mark.parametrize("path,expected_split", [
     (
         ["foo.py", "<<functions>>"],
@@ -141,6 +168,23 @@ def test_split_legacy_path(path, expected_split):
 ])
 def test_legacy_code_text_to_fragments(text, expected_fragments):
     assert legacy_code_text_to_fragments(text) == expected_fragments
+def test_converts_legacy_root_document(tmpfile):
+    root_page = {
+        "title": "Root",
+        "children": [],
+        "id": "abc1",
+        "paragraphs": [],
+    }
+    assert write_read_document(tmpfile, root_page) == {
+        "root_page": root_page,
+        "variables": {},
+    }
+def write_read_document(tmpfile, document):
+    write_json_to_file(tmpfile, document)
+    doc = Document.from_file(tmpfile)
+    with doc.notify():
+        pass
+    return load_json_from_file(tmpfile)
 def test_reads_legacy_scratch_pages(tmpfile):
     write_json_to_file(tmpfile, {
         "workspace": {
