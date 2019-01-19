a7a93307e1838592df6f7773835a29f8f9f62389
Rickard Lindberg <ricli85@gmail.com>
2019-01-19 12:38:15 +0100

    Remove obsolete script that assembled rliterate.py. (Now don inside rliterate.)

--- splice.py
+++
@@ -1,4 +0,0 @@
-import sys
-template = open(sys.argv[1]).read()
-widgets  = open(sys.argv[2]).read()
-sys.stdout.write(template.replace("\n### INSERT GENERATED GUI CLASSES HERE ###\n", "\n"+widgets))
